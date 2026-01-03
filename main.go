d6edcc50e6eceec41549ab8185717dedecc5db1e
tmakinde <makindet74@gmail.com>
2026-01-03 09:40:11 +0100

    chore: Add factorial exercise

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 func main() {
 	fmt.Println(divisible(2000, 3200))
+	fmt.Println(factorial(8))
 }
 
 /*
@@ -27,4 +28,19 @@ func divisible(min int, max int) string {
 		}
 	}
 	return result.String()
+}
+
+/*
+Write a program which can compute the factorial of a given numbers. The results should be printed in a comma-separated sequence on a single line.
+*/
+func factorial(n int) int {
+	if n == 0 || n == 1 || n < 0 {
+		return 1
+	}
+
+	result := 1
+	for i := n; i >= 1; i-- {
+		result *= i
+	}
+	return result
 }
