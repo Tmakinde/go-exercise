36f3d5bcfd42a03c9a31877b4eacbc29382bebaa
tmakinde <makindet74@gmail.com>
2026-01-05 19:06:31 +0100

    Add gin module; Basic API endpoints

--- main.go
+++ main.go
@@ -6,17 +6,57 @@ import (
 	"strings"
 	"os"
 	"bufio"
+	"net/http"
+    "github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println(divisible(2000, 3200))
-	fmt.Println(factorial(8))
-	fmt.Printf("%v", "Squares: ")
-	fmt.Printf("%v", squaredNumber(5))
-	basicFileOperations()
-	result, _ := readFromFile()
-	fmt.Println(result)
-	appendToFile()
+	router := gin.Default()
+    router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbum)
+    router.Run("localhost:8080")
+}
+
+type album struct {
+    ID     string  `json:"id"`
+    Title  string  `json:"title"`
+    Artist string  `json:"artist"`
+    Price  float64 `json:"price"`
+}
+
+// albums slice to seed record album data.
+var albums = []album{
+    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+	id := c.Query("id")
+
+	if id != "" {
+		for _, a := range albums {
+			if a.ID == id {
+				c.IndentedJSON(http.StatusOK, a)
+				return
+			}
+		}
+	}
+    c.IndentedJSON(http.StatusOK, albums)
+}
+
+func postAlbum(c *gin.Context) {
+	var newAlbum album
+
+	// bind incoming JSON to newAlbum
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// add new album to slice
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
 /*
